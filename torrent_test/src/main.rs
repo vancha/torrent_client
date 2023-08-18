46c398e9d55c9cf55db91c7cbe26fbcdb2eef0a1
Vancha <tjipkevanderheide90@gmail.com>
2023-08-18 14:39:24 +0200

    Update main.rs

--- torrent_test/src/main.rs
+++ torrent_test/src/main.rs
@@ -1,24 +1,32 @@
-use std::path::Path;
-use std::fs;
-use std::collections::HashMap;
 use lava_torrent::torrent::v1::Torrent;
-use urlencoding::{encode_binary, encode};
+use lava_torrent::tracker::TrackerResponse;
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+use urlencoding::{encode, encode_binary};
 
+use std::net::TcpStream;
 
+//for use with the tcpstream
+use std::io::Read;
+use std::io::Write;
 
 fn main() {
-    let torrent_location = Path::new("/home/vancha/Documents/rust/torrent_test/ubuntu.torrent");   
-    
+    /*
+     * getting the data from the torrent file
+     */
+    let torrent_location = Path::new("/home/vancha/Documents/rust/torrent_test/ubuntu.torrent");
+
     let parsed_torrent = Torrent::read_from_file(torrent_location).unwrap();
 
     //here we have the announce url(s)
-    let announce_url    = parsed_torrent.announce.clone().unwrap();
+    let announce_url = parsed_torrent.announce.clone().unwrap();
 
     //here we have the info hash
-    let info_hash       = parsed_torrent.info_hash_bytes();
+    let info_hash = parsed_torrent.info_hash_bytes();
     let urlencoded_info_hash = encode_binary(&info_hash);
     println!("info hash: {:?}", info_hash);
-    
+
     let peer_id = encode("thurmanmermadddddddn");
     let port = 6881;
     let uploaded = 0;
@@ -26,18 +34,107 @@ fn main() {
     let left = parsed_torrent.length;
     let event = "started";
 
+    /*
+     * performing the request to the tracker for peers
+     */
     let complete_request_url = format!("{announce_url}?info_hash={urlencoded_info_hash}&peer_id={peer_id}&port={port}&uploaded={uploaded}&downloaded={downloaded}&left={left}&event={event}");
     //client must announce himself by sending a get request to this tracker anounce url like
     //so: http://some.tracker.com:999/announce?info_hash=12345678901234567890&peer_id=ABCDEFGHIJKLMNOPQRST&ip=255.255.255.255&port=6881&downloaded=1234&left=98765&event=stopped
-    //here we perform the actual request 
-    let resp = reqwest::blocking::get(complete_request_url).unwrap(); 
+    //
+    //in this response there is
+    //here we perform the actual request
+    let resp = reqwest::blocking::get(complete_request_url).unwrap();
     //if successful, this holds the tracker response, bencoded something that looks like this:
+
+    let response_as_bytes = resp.bytes().unwrap();
+    //println!("reponse as bytes: {:?}",response_as_bytes);
+    let tracker_response =
+        lava_torrent::tracker::TrackerResponse::from_bytes(response_as_bytes).unwrap();
+    println!("tracker response: {:?}", tracker_response);
+
+    println!(
+        "nr. of pieces: {:?},total size in bytes: {}, without last piece it's {} in size",
+        parsed_torrent.pieces.len(),
+        parsed_torrent.length,
+        (parsed_torrent.pieces.len() - 1) * parsed_torrent.piece_length as usize
+    );
+
     /*
-     * "d8:completei507e10:incompletei16e8:intervali1800e5:peersld2:ip19:2607:5300:60:623::17:peer id20:-TR2940-bqvtv6y7pqj74:porti51413eed2:ip22:2a02:c206:3008:6973::17:peer id20:-TR3000-c0axclo9rhkv4:porti51413eed2:ip25:2a02:6f8:2020:203:3::10037:peer id20:-TR3000-0lr7xdeum3fv4:porti51413eed2:ip39:2403:6200:8840:8ddb:d513:1f69:1387:6b897:peer id20:-FD51]�-m)(SMfzhQLRv4:porti49160eed2:ip19:2001:41d0:a:62c9::17:peer id20:-TR3000-96rathvd2r094:porti60674eed2:ip35:2a01:e0a:bf1:53b0:7ee3:98:9a8b:70967:peer id20:-TR2940-a39kg9xj0rmc4:porti51413eed2:ip20:2a01:4f8:241:4e4a::27:peer id20:-TR410Z-dlzcmrwpleff4:porti51413eed2:ip14:185.125.190.597:peer id20:T03I--00S9efKsGa0kXV4:porti6899eed2:ip39:2607:fea8:fdf0:80f1:3dce:e4fd:b934:e2c17:peer id20:-TR2940-fub0vk68m6id4:porti51413eed2:ip21:2a01:260:1027:3::cef27:peer id20:-TR3000-zzfz4fwu2h0w4:porti33333eed2:ip36:2a01:36d:115:c53e:f4eb:6ac:50d5:32617:peer id20:-UT2210-�b�(S����F-\u{17}4:porti13142eed2:ip20:2607:5380:c002:1b::37:peer id20:-TR2940-ne8f3n0qlg8n4:porti51413eed2:ip19:2001:41d0:8:d92e::17:peer id20:-TR2920-iu34apntk8yc4:porti51413eed2:ip38:2a02:2698:902a:17fe:208:9bff:fece:443d7:peer id20:-TR2940-v8dpxv3vbwoa4:porti51413eed2:ip19:2a01:4f8:10b:214::27:peer id20:-lt0D80-�P�ߎ�k�{�Ȅ4:porti6974eeee"
+     * set up a tcp connection with those peers
      */
 
-    let response_as_bytes =  resp.bytes().unwrap();
-    println!("reponse as bytes: {:?}",response_as_bytes);
-    let tracker_response = lava_torrent::tracker::TrackerResponse::from_bytes(response_as_bytes);
-    println!("tracker response: {:?}", tracker_response);
+    let peers = match tracker_response {
+        TrackerResponse::Success {
+            interval,
+            peers,
+            warning,
+            min_interval,
+            tracker_id,
+            complete,
+            incomplete,
+            extra_fields,
+        } => {
+            peers
+            //println!("successfully retrieved all data from tracker, peers: {:?}", peers);
+        }
+        _ => {
+            vec![]
+            //println!("fail");
+        }
+    };
+
+    for peer in peers {
+        match TcpStream::connect(peer.addr) {
+            Ok(mut stream) => {
+                println!("Connected to server");
+                loop {
+                    //handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
+                    //for v1.0 of torrent protocol, pstrlen = 19 and pstr = BitTorrent protocol
+                    let pstrlen = "19";
+                    let pstr = "BitTorrent protocol";
+                    let reserved = "00000000";
+                    //info hash we have already defined above
+                    //same for peer id
+
+                    let mut request: Vec<u8> = [
+                        &[0x13],
+                        pstr.as_bytes(),
+                        reserved.as_bytes(),
+                        &info_hash,
+                        &*peer_id.as_bytes(),
+                    ]
+                    .concat();
+                    println!(
+                        "request looks like: {:?}, and has size of {} bytes",
+                        request,
+                        request.len()
+                    );
+                    stream.write(&request);
+                    let mut read_buffer = &mut [0; 128];
+                    println!(
+                        "The tcpstream responded with: {}",
+                        stream.read(read_buffer).unwrap()
+                    );
+                    println!("contents of response: {:?}", read_buffer);
+                    let their_info_hash = &read_buffer[28..48];
+                    //make sure hashes match, so we know that this peer and us are talking about
+                    //the same file
+                    if info_hash == their_info_hash {
+                        println!("our little info hashes match");
+                    }
+                    //break;
+                }
+            }
+            Err(e) => {
+                println!("Couldn't connect to server {} because {:?}", peer.addr, e);
+            }
+        }
+    }
+    /*
+     *  perform handshakes with peers
+     */
+
+    /*
+     *  download pieces
+     */
 }
