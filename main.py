bec074af17a09897bb9b61655a62fcac8d355b88
Vancha <tjipkevanderheide90@gmail.com>
2024-05-16 20:04:49 +0200

    Removed extraneous checking of additional data
    looks like torrent files only have a single dictionary that needs to be encoded?

--- main.py
+++ main.py
@@ -4,24 +4,17 @@ class bdecoder:
 
     def decode_byte_string(self, byte_iterator):
         #byte strings are prefixed with a length
-        length = []
+        length = ''
         
-        while True:
-            #get the whole number that represents the length
-            if chr(byte_iterator.peek()).isnumeric():
-                length.append(chr(byte_iterator.peek()))
-                next(byte_iterator)
-            else:
-                #also skip the double colon that's present after every string length
-                next(byte_iterator)
-                break
+        while chr(byte_iterator.peek()).isnumeric():
+            length += chr(next(byte_iterator))
+        
+        #also skip the double colon that's present after every string length
+        next(byte_iterator)
         value = ""
-
-        for _ in range(int(''.join(length))):
-            value = value + chr(byte_iterator.peek())
-            next(byte_iterator)
+        for _ in range(int(length)):
+            value += chr(next(byte_iterator))
         return value
-
 
     def decode_integer(self, byte_iterator):
         #skip over the 'i'
@@ -29,25 +22,27 @@ class bdecoder:
         values = ''
          
         while not chr(byte_iterator.peek()) == 'e':#.isnumeric():
-            values = ''.join([values, chr(next(byte_iterator))])
+            values += chr(next(byte_iterator))
         
         #skip over the 'e'
         next(byte_iterator)
         return int(values)
-        
 
     def decode_list(self, byte_iterator):
         #skip over the 'l'
         next(byte_iterator)
+
         result = []
         while not chr(byte_iterator.peek()) == 'e':
             result.append(self.decode_next(byte_iterator))
+
         #skip over the 'e'
         next(byte_iterator)
         return result
 
     def decode_next(self, byte_iterator):
         val = chr(byte_iterator.peek())
+
         if val == 'd':
             return self.decode_dict(byte_iterator)
         elif val == 'l':
@@ -58,13 +53,15 @@ class bdecoder:
             return self.decode_byte_string(byte_iterator)
 
     def decode_dict(self, byte_iterator):
-        #skip over the 'd'
+        #skip over the initial 'd'
         next(byte_iterator)
+
         result  = {}
         while not chr(byte_iterator.peek()) == "e":
             field_name =  self.decode_byte_string(byte_iterator)
             field_value = self.decode_next(byte_iterator)
             result[field_name] = field_value
+
         #skip over the 'e'
         next(byte_iterator)
         return result
@@ -74,14 +71,9 @@ class bdecoder:
         file  = open(torrent_file, "rb").read()
         byte_iterator  = peekable(file)
         results = []
-        try:
-            while True:
-                results.append(self.decode_next(byte_iterator))
-        except StopIteration:
-            print('reached end of file')
-            return results
-
-
+        return self.decode_next(byte_iterator)
+    
 x = bdecoder()
 
-print(x.decode("./ubuntu.torrent"))
+decoded_dict = x.decode("./ubuntu.torrent")
+print(decoded_dict["announce-list"])
