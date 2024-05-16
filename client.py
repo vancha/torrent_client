283b5fdcae3a5cb5e5b8c3d93e1de6aff3fcee12
Vancha <tjipkevanderheide90@gmail.com>
2024-05-16 14:24:48 +0200

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-class Client:
-    #takes location to a torrent file, attempts to parse it
-    def __init__(self, torrent_file_location):
-        #the parsed meta info file
-        meta_info_file  = MetaInfo(torrent_file_location)
-        #the tracker to get the peers from
-        tracker         = Tracker()
-        #gets the tracker response, holds the peer list if successful
-        response        = tracker.get_response()
