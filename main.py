9411f47bea818c87f3728d84001a7c0afe442d77
Vancha <tjipkevanderheide90@gmail.com>
2024-05-17 09:50:17 +0200

    Added better comments

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
+#used to see what the next byte in the torrent file is
 from more_itertools import peekable
 
+'''
+A python class that can decode a torrent file
+Call bdecoder.decode( torrent_file_location )  to use it.
+'''
 class bdecoder:
-
-    def decode_byte_string(self, byte_iterator):
-        #byte strings are prefixed with a length
+    '''
+    Assumes the next(byte_iterator) returns the start of a string
+    Moves forward in the iterator and returns the string
+    '''
+    def decode_byte_string(byte_iterator):
+        #byte strings are length prefixed, get the length first
         length = ''
-        
+         
         while chr(byte_iterator.peek()).isnumeric():
             length += chr(next(byte_iterator))
         
-        #also skip the double colon that's present after every string length
+        #byte strings are suffixed with a :, skip over that before returning
         next(byte_iterator)
-        value = ""
+        result = ""
         for _ in range(int(length)):
-            value += chr(next(byte_iterator))
-        return value
-
-    def decode_integer(self, byte_iterator):
+            result += chr(next(byte_iterator))
+        return result
+    
+    '''
+    Assumes the next(byte_iterator) returns the start of an integer
+    Moves forward in the iterator and returns the integer
+    '''
+    def decode_integer(byte_iterator):
         #skip over the 'i'
         next(byte_iterator)
         values = ''
@@ -28,52 +40,66 @@ class bdecoder:
         next(byte_iterator)
         return int(values)
 
-    def decode_list(self, byte_iterator):
+    '''
+    Assumes the next(byte_iterator) returns the start of a list
+    Moves forward in the iterator and returns the list
+    '''
+    def decode_list( byte_iterator):
         #skip over the 'l'
         next(byte_iterator)
 
         result = []
         while not chr(byte_iterator.peek()) == 'e':
-            result.append(self.decode_next(byte_iterator))
+            result.append(bdecoder.decode_next(byte_iterator))
 
         #skip over the 'e'
         next(byte_iterator)
         return result
-
-    def decode_next(self, byte_iterator):
+    
+    '''
+    Deduces the type of the next object in the iterator and returns it
+    '''
+    def decode_next(byte_iterator):
         val = chr(byte_iterator.peek())
 
         if val == 'd':
-            return self.decode_dict(byte_iterator)
+            return bdecoder.decode_dict(byte_iterator)
         elif val == 'l':
-            return self.decode_list(byte_iterator)
+            return bdecoder.decode_list(byte_iterator)
         elif val == 'i':
-            return self.decode_integer(byte_iterator)
+            return bdecoder.decode_integer(byte_iterator)
         else:
-            return self.decode_byte_string(byte_iterator)
-
-    def decode_dict(self, byte_iterator):
+            return bdecoder.decode_byte_string(byte_iterator)
+    
+    '''
+    Assumes the next(byte_iterator) returns the start of a dictionary
+    Moves forward in the iterator and returns the dictionary
+    '''
+    def decode_dict(byte_iterator):
         #skip over the initial 'd'
         next(byte_iterator)
 
         result  = {}
         while not chr(byte_iterator.peek()) == "e":
-            field_name =  self.decode_byte_string(byte_iterator)
-            field_value = self.decode_next(byte_iterator)
+            field_name =  bdecoder.decode_byte_string(byte_iterator)
+            field_value = bdecoder.decode_next(byte_iterator)
             result[field_name] = field_value
 
         #skip over the 'e'
         next(byte_iterator)
         return result
 
-    
-    def decode(self, torrent_file):
+    '''
+    Takes the torrent file, and attempts to parse it.
+    returns the parsed value
+    '''
+    def decode(torrent_file):
         file  = open(torrent_file, "rb").read()
         byte_iterator  = peekable(file)
-        results = []
-        return self.decode_next(byte_iterator)
+        return bdecoder.decode_next(byte_iterator)
 
-x = bdecoder()
-
-decoded_dict = x.decode("./ubuntu.torrent")
-print(decoded_dict["announce-list"])
+'''
+Example usage:
+    decoded_file = bdecoder.decode("./example.torrent")
+    print(decoded_file["info"])
+'''
