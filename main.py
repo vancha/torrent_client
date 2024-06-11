782753034265502be79d542e725653b404453b48
Vancha <tjipkevanderheide90@gmail.com>
2024-06-11 16:22:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ import  hashlib
 from    urllib          import request
 #to perform urlencoding on for example the info_hash
 from    urllib.parse    import quote
-import pprint
-
+import socket
+from ipaddress import ip_address, IPv4Address 
+  
+def is_ipv4(ip: str):
+    if type(ip_address(ip)) is IPv4Address:
+        return True
+    else:
+        return False
 
 PEER_ID = "thurmanmermanddddddd"
 
@@ -20,9 +26,26 @@ tracker = Tracker( mif, PEER_ID )
 #send the http "GET" request to the tracker to get list of peers
 
 
+#attempts to get peers from tracker
+response = tracker.get_peers()
+#get peers from the response, just assume this was successful
+peers = list(bdecoder.decode(response).values())[3]
 
-peers = tracker.get_peers()
-print(pprint.pprint(bdecoder.decode(peers)))
-#print(f'peers: {peers}')
+for peer in peers:
+    peer_id     = peer[b"peer id"]
+    peer_ip     = peer[b"ip"].decode('utf-8')
+    peer_port   = peer[b"port"]
+    #maybe focus on ipv4 for now, forget v6
+    if is_ipv4(peer_ip):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                #s.connect((peer_ip, peer_port))
+                print(f'this is the part where we need to perform a handshake with the peer')
+            except TimeoutError:
+                print('timeout')
+            except InterruptedError:
+                print('interrupted')
+        print('done with the connection, i got nothing')
 
+print('exit')
 
