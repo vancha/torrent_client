a4a157f55a75ad5466259331c5143559a63663d0
Vancha <tjipkevanderheide90@gmail.com>
2025-04-18 13:36:26 +0200

    Delete PieceSelector.py

--- PieceSelector.py
+++
@@ -1,8 +0,0 @@
-class PieceSelector:
-    def __init__(self):
-        pass
-    
-    def request_piece_from_peers(self, peers):
-        print(f"requesting pieces from the list of peers")
-        pass
-    
